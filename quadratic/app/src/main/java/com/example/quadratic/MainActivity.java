9e56be87f0ae530cc4135770cbad78b1dbc6c6fe
Nida909 <nidaanjum440@gmail.com>
2020-11-06 21:39:42 +0500

    andriod project commit

--- quadratic/app/src/main/java/com/example/quadratic/MainActivity.java
+++ quadratic/app/src/main/java/com/example/quadratic/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
  EditText tt;
  TextView tv;
  String s1,s2,s3;
+
     int a,b,c,res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
     public void root(View v)
     {
+        boolean op =false;
         ed=(EditText)findViewById(R.id.edt);
         et=(EditText)findViewById(R.id.edt1);
         tt=(EditText)findViewById(R.id.edt2);
@@ -38,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
             boolean opt=Character.isDigit(s1.charAt(i));
             if(opt==false)
             {
-                Toast to=Toast.makeText(getApplicationContext(),"Enter any Integer",Toast.LENGTH_SHORT);
+                Toast to=Toast.makeText(getApplicationContext(),"Enter Integer Value for a",Toast.LENGTH_SHORT);
                 to.setMargin(50,50);
                 to.show();
-                ed.setText(" ");
+
+                op=true;
+                break;
             }
         }
         for(int i=0;i<s2.length();i++)
@@ -49,10 +53,12 @@ public class MainActivity extends AppCompatActivity {
             boolean opt=Character.isDigit(s2.charAt(i));
             if(opt==false)
             {
-                Toast to=Toast.makeText(getApplicationContext(),"Enter any Integer",Toast.LENGTH_SHORT);
+                Toast to=Toast.makeText(getApplicationContext(),"Enter Integer value for b",Toast.LENGTH_SHORT);
                 to.setMargin(50,50);
                 to.show();
-                et.setText(" ");
+
+                op=true;
+                break;
             }
         }
         for(int i=0;i<s3.length();i++)
@@ -60,34 +66,36 @@ public class MainActivity extends AppCompatActivity {
             boolean opt=Character.isDigit(s3.charAt(i));
             if(opt==false)
             {
-                Toast to=Toast.makeText(getApplicationContext(),"Enter any Integer",Toast.LENGTH_SHORT);
+                Toast to=Toast.makeText(getApplicationContext(),"Enter Integer value for c",Toast.LENGTH_SHORT);
                 to.setMargin(50,50);
                 to.show();
-                tt.setText(" ");
+
+                op=true;
+                break;
             }
         }
         tv=(TextView) findViewById(R.id.t);
-        a=Integer.parseInt(s1);
-        b=Integer.parseInt(s2);
-        c=Integer.parseInt(s3);
-        res=(b*b)-(4*a*c);
-        if(res<0)
-        {
-            Toast to=Toast.makeText(getApplicationContext(),"Root is Imaginary",Toast.LENGTH_SHORT);
-            to.setMargin(50,50);
-            to.show();
-            tv.setText("Root is Imaginary ");
-            tv.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            double x,y;
-            x=((-b)-(Math.sqrt(res)))/(2*a);
-            y=((-b)+(Math.sqrt(res)))/(2*a);
-            tv.setText("Root = "+ x +" , "+ y);
-            tv.setVisibility(View.VISIBLE);
+        if(op==false) {
+            a = Integer.parseInt(s1);
+            b = Integer.parseInt(s2);
+            c = Integer.parseInt(s3);
+            res = (b * b) - (4 * a * c);
+            if (res < 0) {
+                Toast to = Toast.makeText(getApplicationContext(), "Root is Imaginary", Toast.LENGTH_SHORT);
+                to.setMargin(50, 50);
+                to.show();
+                tv.setText("Root is Imaginary ");
+                tv.setVisibility(View.VISIBLE);
+            } else {
+                double x, y;
+                x = ((-b) - (Math.sqrt(res))) / (2 * a);
+                y = ((-b) + (Math.sqrt(res))) / (2 * a);
+                tv.setText("Root = " + x + " , " + y);
+                tv.setVisibility(View.VISIBLE);
 
 
+
+            }
         }
     }
 }
